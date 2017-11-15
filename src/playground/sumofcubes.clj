124788d23fe6ab4a622531330d74c5402e8a4775
Leira Hua <leirahua@gmail.com>
2017-11-15 00:19:23 -0800

    codewars kata Build a pile of Cubes

---
+++ src/playground/sumofcubes.clj
@@ -0,0 +1,16 @@
+(ns playground.sumofcubes)
+
+;; https://www.codewars.com/kata/build-a-pile-of-cubes/
+
+(defn- guess-n [m]
+  (bigint (Math/sqrt (* (Math/sqrt m) 2))))
+
+(defn- sum-cubes [n]
+  (let [x (/ (* n (+ n 1)) 2)]
+    (* x x)))
+
+(defn find-nb [m]
+  (let [n (guess-n m)]
+    (if (= (sum-cubes n) m)
+        n
+        -1)))
