10a8c50ee1c5165a8db2606ef002e8b381ec0797
Leira Hua <leirahua@gmail.com>
2017-11-28 23:08:14 -0800

    memoized version, still too slow...

--- src/playground/dblinear.clj
+++ src/playground/dblinear.clj
@@ -5,9 +5,8 @@
 (defn yf [x] (inc (* x 2)))
 (defn zf [x] (inc (* x 3)))
 
-(defn gen-dbls
-  ([] (cons 1 (lazy-seq (gen-dbls [1] []))))
-  ([bases tbd]
+(def gen-dbls-m
+  (memoize (fn [bases tbd]
     (let [x (first bases)
           y (yf x)
           z (zf x)
@@ -15,11 +14,15 @@
           determined (subseq ntbd <= y)]
       (concat determined 
             (lazy-seq
-             (gen-dbls (concat (rest bases) determined)
-                       (subseq ntbd > y)))))))
+             (gen-dbls-m (concat (rest bases) determined)
+                       (subseq ntbd > y))))))))
+
+(defn gen-dbls []
+  (cons 1 (lazy-seq (gen-dbls-m [1] []))))
 
 (defn dblinear [n]
     (last (take (inc n) (gen-dbls))))
 
-;(take 10 (gen-dbls))
-(dblinear 1000)
+;(take 100 (gen-dbls))
+(do (time (dblinear 2000))
+    (time (dblinear 2000)))
