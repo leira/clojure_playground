f1cfe84699fd31060c282f0f04da087a6c107f82
Leira Hua <leirahua@gmail.com>
2017-12-01 15:38:18 -0800

    Improved performance, got rid of sorted-set.
    1. Naive version in 4a3fee4, (dblinear 2000) took 3072.780391 msecs
    2. Cut off on 2x+1 in ee7c751, (dblinear 2000) took 264.07242 msecs
    3. Got rid of sorted-set, (dblinear 2000) took 18.233228 msecs
    
    But still timeout on Codewars...

--- src/codewars/dblinear.clj
+++ src/codewars/dblinear.clj
@@ -5,24 +5,37 @@
 (defn yf [x] (inc (* x 2)))
 (defn zf [x] (inc (* x 3)))
 
-(def gen-dbls-m
-  (memoize (fn [bases tbd]
+(defn split-before
+  ([coll y m] (split-before [] coll y m))
+  ([before coll y m]
+    ;(println "split-before" before coll y m)
+    (if (zero? m)
+        [before coll 0]
+        (let [n (first coll)]
+          (cond (or (nil? n) (> n y))
+                  [(conj before y) coll (dec m)]
+                (= n y) [(conj before y) (rest coll) (dec m)]
+                :esle (recur (conj before (first coll))
+                             (rest coll)
+                             y
+                             (dec m)))))))
+
+(defn dblinear
+  ([n] (if (zero? n) 1 (dblinear [1] [] n)))
+  ([bases tpls m]
+    ;(println bases tpls m)
     (let [x (first bases)
           y (yf x)
-          z (zf x)
-          ntbd (apply sorted-set (conj tbd y z))
-          determined (subseq ntbd <= y)]
-      (concat determined 
-            (lazy-seq
-             (gen-dbls-m (concat (rest bases) determined)
-                       (subseq ntbd > y))))))))
+          [dtmed ntpls nm] (split-before tpls y m)]
+      (if (zero? nm)
+          (peek dtmed)
+          (recur (into (vec (rest bases)) dtmed)
+                 (conj (vec ntpls) (zf x))
+                 nm)))))
+;  (last (take (inc n) (gen-dbls))))
 
-(defn gen-dbls []
-  (cons 1 (lazy-seq (gen-dbls-m [1] []))))
+;(split-before [3 5 9 19] 10 3)
+;(split-before [] 3 1000)
 
-(defn dblinear [n]
-    (last (take (inc n) (gen-dbls))))
-
-;(take 100 (gen-dbls))
-(do (time (dblinear 2000))
-    (time (dblinear 2000)))
+;(time (map dblinear (range 100)))
+(time (dblinear 2000))
