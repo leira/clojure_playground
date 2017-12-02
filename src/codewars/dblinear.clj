4e199a5616a3ed388a7205c0a6749ba0876695cf
Leira Hua <leirahua@gmail.com>
2017-12-01 17:26:21 -0800

    using split-with, (dblinear 2000) took 21.517628 msecs, slower than previous version

--- src/codewars/dblinear.clj
+++ src/codewars/dblinear.clj
@@ -5,37 +5,24 @@
 (defn yf [x] (inc (* x 2)))
 (defn zf [x] (inc (* x 3)))
 
-(defn split-before
-  ([coll y m] (split-before [] coll y m))
-  ([before coll y m]
-    ;(println "split-before" before coll y m)
-    (if (zero? m)
-        [before coll 0]
-        (let [n (first coll)]
-          (cond (or (nil? n) (> n y))
-                  [(conj before y) coll (dec m)]
-                (= n y) [(conj before y) (rest coll) (dec m)]
-                :esle (recur (conj before (first coll))
-                             (rest coll)
-                             y
-                             (dec m)))))))
-
 (defn dblinear
-  ([n] (if (zero? n) 1 (dblinear [1] [] n)))
+  ([n] (if (zero? n) 1 (dblinear [1] [] (dec n))))
   ([bases tpls m]
-    ;(println bases tpls m)
+    ;(println "dblinear" bases tpls m)
     (let [x (first bases)
           y (yf x)
-          [dtmed ntpls nm] (split-before tpls y m)]
-      (if (zero? nm)
-          (peek dtmed)
-          (recur (into (vec (rest bases)) dtmed)
-                 (conj (vec ntpls) (zf x))
-                 nm)))))
-;  (last (take (inc n) (gen-dbls))))
+          [l r] (split-with (partial > y) tpls)
+          dtmd (conj (vec l) y)]
+      ;(println x y l r m)
+      (if (> (count dtmd) m)
+          (dtmd m)
+          (recur (into (vec (rest bases)) dtmd)
+                 (conj (vec (if (= (first r) y)
+                                (rest r)
+                                r))
+                       (zf x))
+                 (- m (count dtmd)))
+      ))))
 
-;(split-before [3 5 9 19] 10 3)
-;(split-before [] 3 1000)
-
-;(time (map dblinear (range 100)))
+;(time (map dblinear (range 10)))
 (time (dblinear 2000))
