367c751ab9ad7607ddd3dd528f0419f4ea37d1ab
Leira Hua <leirahua@gmail.com>
2017-12-01 20:55:06 -0800

    switched to use clojure.lang.PersistentQueue/EMPTY, finally passed

--- src/codewars/dblinear.clj
+++ src/codewars/dblinear.clj
@@ -8,18 +8,25 @@
 (defn yf [x] (inc (* x 2)))
 (defn zf [x] (inc (* x 3)))
 
+(defn append [q coll]
+  (if (seq coll)
+      (recur (conj q (first coll))
+      (rest coll)) q))
+
 (defn dblinear
-  ([n] (if (zero? n) 1 (dblinear [1] [] (dec n))))
-  ([bases tpls m]
+  ([n] (if (zero? n) 1 (dblinear (conj clojure.lang.PersistentQueue/EMPTY 1)
+                                 []
+                                 (dec n))))
+    ([bases tpls m]
     ;(println "dblinear" bases tpls m)
-    (let [x (first bases)
+    (let [x (peek bases)
           y (yf x)
           [l r] (p :split (split-with (partial > y) tpls))
           dtmd (p :dtmd (conj (vec l) y))]
       ;(println x y l r m)
       (if (> (count dtmd) m)
           (dtmd m)
-          (recur (p :new-base (into (vec (rest bases)) dtmd))
+          (recur (p :new-base (append (pop bases) dtmd))
                  (p :new-tpls (conj (vec (if (= (first r) y)
                                 (rest r)
                                 r))
@@ -28,4 +35,4 @@
       ))))
 
 ;(time (map dblinear (range 10)))
-(profile {} (dblinear 2000))
+(profile {} (dblinear 20000))
