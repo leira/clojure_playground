d9b3615413da68a0c0f010bc21f3ec8738ab2b18
Leira Hua <leirahua@gmail.com>
2017-12-11 18:07:53 -0800

    codewars kata rainfall

---
+++ src/codewars/rainfall.clj
@@ -0,0 +1,31 @@
+(ns codewars.rainfall
+  (:require [clojure.string :as str]))
+
+(defn parse-ln [ln]
+  (let [[town hist] (str/split ln #":")]
+    [town
+      (->> (str/split hist #",")
+          (map #(second (str/split % #" ")))
+          (map read-string))]))
+
+(defn parse-data [data]
+  (apply hash-map
+         (mapcat parse-ln
+                 (str/split-lines data))))
+                 
+(defn mean* [nums]                 
+  (/ (apply + nums) (count nums)))
+  
+(defn variance* [nums]
+  (/ (apply + (map #(* % %) nums)) (count nums)))
+
+(defn apply-town [fn twn strng]
+  (if-let [nums (get (parse-data strng) twn)]
+    (fn nums)
+    -1))
+
+(defn mean [twn strng]
+  (apply-town mean* twn strng))
+  
+(defn variance [twn strng] 
+  (apply-town variance* twn strng)) 
